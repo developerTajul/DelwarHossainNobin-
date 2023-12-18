4ac0d74912262197d874b662a669dff4b46d5628
DelwarHosssain <mddelwarhossainnobin12@gmail.com>
2023-12-19 01:06:39 +0600

    main-part-complet

---
+++ monu-projecet/js/app.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.active-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        speed: 300,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay:true,
+    });
+});
